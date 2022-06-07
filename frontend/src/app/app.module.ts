ecb2b892c6a39593a50c09d2a3cdc725aee96a84
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2022-06-07 20:31:14 -0300

    Criação dos componentes funcionario (create, delete, read, update)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from "./components/templates/sidebar/sidebar.component";
+import { FuncionarioCreateComponent } from './components/funcionario/funcionario-create/funcionario-create.component';
+import { FuncionarioReadComponent } from './components/funcionario/funcionario-read/funcionario-read.component';
+import { FuncionarioUpdateComponent } from './components/funcionario/funcionario-update/funcionario-update.component';
+import { FuncionarioDeleteComponent } from './components/funcionario/funcionario-delete/funcionario-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    FuncionarioCreateComponent,
+    FuncionarioReadComponent,
+    FuncionarioUpdateComponent,
+    FuncionarioDeleteComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/funcionario/funcionario-create/funcionario-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario-create',
+  templateUrl: './funcionario-create.component.html',
+  styleUrls: ['./funcionario-create.component.scss']
+})
+export class FuncionarioCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
