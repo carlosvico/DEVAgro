569c10f7982aeab2532964c5a44671079fb1df0b
Eduardo <eduardoamorim927@gmail.com>
2022-06-07 18:38:59 -0300

    Refatoração da estrutura do projeto

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { SidebarComponent } from './components/templates/sidebar/sidebar.component'
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: SidebarComponent,
+    path: '/sidebar'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
