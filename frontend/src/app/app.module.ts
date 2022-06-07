bd6497a8675991f646e23aa0d8ec54c20d3edfb8
rafael.ferrari <44851591+rrrafaelll@users.noreply.github.com>
2022-06-07 20:48:43 -0300

    Refatoracao do componente grao

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { FazendaCreateComponent } from './components/fazenda/fazenda-create/faze
 import { FazendaReadComponent } from './components/fazenda/fazenda-read/fazenda-read.component';
 import { FazendaUpdateComponent } from './components/fazenda/fazenda-update/fazenda-update.component';
 import { FazendaDeleteComponent } from './components/fazenda/fazenda-delete/fazenda-delete.component';
+import { GraoCreateComponent } from './components/grao/grao-create/grao-create.component';
+import { GraoUpdateComponent } from './components/grao/grao-update/grao-update.component';
+import { GraoDeleteComponent } from './components/grao/grao-delete/grao-delete.component';
+import { GraoReadComponent } from './components/grao/grao-read/grao-read.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { FazendaDeleteComponent } from './components/fazenda/fazenda-delete/faze
     FazendaCreateComponent,
     FazendaReadComponent,
     FazendaUpdateComponent,
-    FazendaDeleteComponent
+    FazendaDeleteComponent,
+    GraoCreateComponent,
+    GraoUpdateComponent,
+    GraoDeleteComponent,
+    GraoReadComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/grao/grao-update/grao-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grao-update',
+  templateUrl: './grao-update.component.html',
+  styleUrls: ['./grao-update.component.scss']
+})
+export class GraoUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
