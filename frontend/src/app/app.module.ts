92c02fd93caed04912b33294753455518c79f612
raguillar89 <renan_aguillarpinho@hotmail.com>
2022-06-07 20:21:50 -0300

    Refatoração de Componente

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from "./components/templates/sidebar/sidebar.component";
+import { FazendaCreateComponent } from './components/fazenda/fazenda-create/fazenda-create.component';
+import { FazendaReadComponent } from './components/fazenda/fazenda-read/fazenda-read.component';
+import { FazendaUpdateComponent } from './components/fazenda/fazenda-update/fazenda-update.component';
+import { FazendaDeleteComponent } from './components/fazenda/fazenda-delete/fazenda-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    FazendaCreateComponent,
+    FazendaReadComponent,
+    FazendaUpdateComponent,
+    FazendaDeleteComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/fazenda/fazenda-read/fazenda-read.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fazenda-read',
+  templateUrl: './fazenda-read.component.html',
+  styleUrls: ['./fazenda-read.component.scss']
+})
+export class FazendaReadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
