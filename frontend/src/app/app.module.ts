171656219bdef2252295704bf221457219055298
Eduardo <eduardoamorim927@gmail.com>
2022-06-08 13:24:36 -0300

    Ajustes e limpreza do código

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,26 +12,19 @@ import { FazendaCreateComponent } from './components/fazenda/fazenda-create/faze
 import { FazendaReadComponent } from './components/fazenda/fazenda-read/fazenda-read.component';
 import { FazendaUpdateComponent } from './components/fazenda/fazenda-update/fazenda-update.component';
 import { FazendaDeleteComponent } from './components/fazenda/fazenda-delete/fazenda-delete.component';
-<<<<<<< HEAD
+
 import { GraoCreateComponent } from './components/grao/grao-create/grao-create.component';
 import { GraoUpdateComponent } from './components/grao/grao-update/grao-update.component';
 import { GraoDeleteComponent } from './components/grao/grao-delete/grao-delete.component';
 import { GraoReadComponent } from './components/grao/grao-read/grao-read.component';
-=======
+
 import { EmpresaCreateComponent } from './components/empresa/empresa-create/empresa-create.component';
 import { EmpresaReadComponent } from './components/empresa/empresa-read/empresa-read.component';
 import { EmpresaUpdateComponent } from './components/empresa/empresa-update/empresa-update.component';
 import { EmpresaDeleteComponent } from './components/empresa/empresa-delete/empresa-delete.component';
-<<<<<<< HEAD
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-=======
-<<<<<<< HEAD
-import { LoginViewComponent } from './views/login-view/login-view.component';
-=======
->>>>>>> 190530f226cd766b53a18882ad1f0f40e42f0fa6
->>>>>>> 5aa5e8d7480f4182ee643e4088dc27d2c3eee396
->>>>>>> d27439457666be77eda71be836cdab85baeaebe4
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginViewComponent } from './views/login-view/login-view.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -44,22 +37,16 @@ import { LoginViewComponent } from './views/login-view/login-view.component';
     FazendaReadComponent,
     FazendaUpdateComponent,
     FazendaDeleteComponent,
-<<<<<<< HEAD
     GraoCreateComponent,
     GraoUpdateComponent,
     GraoDeleteComponent,
-    GraoReadComponent
-=======
+    GraoReadComponent,
     EmpresaCreateComponent,
     EmpresaReadComponent,
     EmpresaUpdateComponent,
-<<<<<<< HEAD
     EmpresaDeleteComponent,
-    LoginViewComponent
-=======
+    LoginViewComponent,
     EmpresaDeleteComponent
->>>>>>> 190530f226cd766b53a18882ad1f0f40e42f0fa6
->>>>>>> 5aa5e8d7480f4182ee643e4088dc27d2c3eee396
   ],
   imports: [
     BrowserModule,
