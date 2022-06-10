810677126e3aa17175b2deae933ce75d2b85da8c
Carlos-Vico <carlos_vieira-costa@estudante.sesisenai.org.br>
2022-06-10 11:55:31 -0300

    Criado HTML/CSS do componente grao-read

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { GraoCreateComponent } from './components/grao/grao-create/grao-create.component';
+import { GraoReadComponent } from './components/grao/grao-read/grao-read.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "grao",
+    component: GraoReadComponent
+  },
+  {
+    path: "grao/create",
+    component:GraoCreateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/components/grao/grao-read/grao-read.component.ts
+++ frontend/src/app/components/grao/grao-read/grao-read.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-grao-read',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraoReadComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+
+  }
+
+  navigateToGrainCrete(): void {
+    this.router.navigate(['/grao/create'])
+    // alert("Teste")
   }
 
 }
