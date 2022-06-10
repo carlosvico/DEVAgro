3b5fbec31c014d75404420401e727798805f75b1
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2022-06-10 20:43:04 -0300

    Regatoração da Entidade Fazenda

--- frontend/src/app/components/fazenda/fazenda.model.ts
+++ frontend/src/app/components/fazenda/fazenda.model.ts
@@ -1,14 +1,14 @@
 export interface Fazenda {
     id?: number
     nome: string
-    endereco?: string
+    //endereco?: string
     estoque?: number
     ultimaColheita: string
-    grao: {
-        id: number
-    },
+    grao: string
+    /*
     empresa?: {
         id: number
     }
+    */
 }
 
