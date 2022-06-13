2cf4c53f5fa58e947621567b2f2b60bc10ea1ae3
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2022-06-13 14:18:11 -0300

    Implementação do HTML/CSS do Funcionário (CREATE, READ, UPDATE, DELETE), implementação do service do Funcionário, implementação da funcionalidade do service com o HTML/CSS do funcionário (CREATE, READ, UPDATE, DELETE) e validação dos campos do funcionário CREATE e UPDATE.

--- frontend/src/app/components/funcionario/funcionario-delete/funcionario-delete.component.ts
+++ frontend/src/app/components/funcionario/funcionario-delete/funcionario-delete.component.ts
@@ -1,4 +1,9 @@
+import { FuncionarioService } from './../funcionario.service';
+import { Funcionario } from './../funcionario.model';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FazendaService } from '../../fazenda/fazenda.service';
+import { Fazenda } from '../../fazenda/fazenda.model';
 
 @Component({
   selector: 'app-funcionario-delete',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FuncionarioDeleteComponent implements OnInit {
 
-  constructor() { }
+  funcionario: Funcionario;
+
+  constructor(private funcionarioService: FuncionarioService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.funcionarioService.readById(id).subscribe(funcionario => {
+      this.funcionario = funcionario;
+    });
   }
 
+  isActive(event): void {
+    if(event.target.checked){
+      this.funcionario.ativo = true;
+    }else{
+      this.funcionario.ativo = false;
+    }
+  }
+
+  deleteFuncionario(): void {
+    this.funcionarioService.delete(this.funcionario.id).subscribe(() => {
+      this.funcionarioService.showMessage('Funcionário Excluído com Sucesso!');
+      this.router.navigate(["/funcionario"]);
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/funcionario'])
+  }
 }
+
+
