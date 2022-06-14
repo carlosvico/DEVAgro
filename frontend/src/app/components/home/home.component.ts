6f0314a45adc34604f1e6fcb2f0423c30e2a9b08
Eduardo <eduardoamorim927@gmail.com>
2022-06-14 09:01:48 -0300

    Refatoração do component home, início da construção da view home

---
+++ frontend/src/app/components/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Fazenda } from '../fazenda/fazenda.model';
+import { FazendaService } from '../fazenda/fazenda.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          { title: 'Card 1', cols: 1, rows: 1 },
+          { title: 'Card 2', cols: 1, rows: 1 },
+          // { title: 'Card 3', cols: 1, rows: 1 },
+          // { title: 'Card 4', cols: 1, rows: 1 }
+        ];
+      }
+
+      return [
+        // { title: 'Card 1', cols: 2, rows: 1 },
+        { title: 'Card 2', cols: 1, rows: 1 },
+        { title: 'Card 3', cols: 1, rows: 2 },
+        // { title: 'Card 4', cols: 1, rows: 1 }
+      ];
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver, private fazendaService: FazendaService) {}
+
+  fazendas: Fazenda[]
+
+  ngOnInit(): void {
+    this.fazendaService.read().subscribe(fazendas => {
+      this.fazendas = fazendas;
+    })
+  }
+}
