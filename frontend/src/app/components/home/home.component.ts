62c62ada53eccd4b3c1e57b09355cd24ccad407c
Eduardo <eduardoamorim927@gmail.com>
2022-06-15 23:20:04 -0300

    Configuração da home, início da configuração da funcionalidade de previsão do tempo

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,6 +1,10 @@
+import { Funcionario } from './../funcionario/funcionario.model';
+import { FuncionarioService } from './../funcionario/funcionario.service';
 import { Component } from '@angular/core';
 import { Fazenda } from '../fazenda/fazenda.model';
 import { FazendaService } from '../fazenda/fazenda.service';
+import { HomeService } from './home.service';
+import { WeatherData } from './weatherModel/weather.model';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +13,28 @@ import { FazendaService } from '../fazenda/fazenda.service';
 })
 export class HomeComponent {
   
-  constructor( private fazendaService: FazendaService) {}
+  constructor( private fazendaService: FazendaService, 
+    private funcionarioService: FuncionarioService, private weatherService: HomeService) {}
 
-  fazendas: Fazenda[]
+  fazendas: Fazenda[];
+  funcionarios: Funcionario[];
+  weatherData?: WeatherData;
 
   ngOnInit(): void {
     this.fazendaService.read().subscribe(fazendas => {
       this.fazendas = fazendas;
     })
+
+    this.funcionarioService.read().subscribe(funcionario => {
+      this.funcionarios = funcionario;
+    })
+
+    this.weatherService.getWeatherData('sao paulo')
+    .subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        console.log(response);
+      }
+    });
   }
 }
