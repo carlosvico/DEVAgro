e957d7203ee3209db7ffa74ee0ca50e1a12d00f7
Guilherme Fortes <guilhermelucianofortes@gmail.com>
2022-06-17 15:47:40 -0300

    Validação do formulário da tela de cadastro de Empresa

--- frontend/src/app/components/empresa/empresa-create/empresa-create.component.ts
+++ frontend/src/app/components/empresa/empresa-create/empresa-create.component.ts
@@ -14,6 +14,8 @@ export class EmpresaCreateComponent implements OnInit {
   passwordType: string = 'password';
   passwordType2: string = 'password';
 
+  empresas: Empresa[];
+
   empresa: Empresa = {
     nome: '',
     email: '',
@@ -23,9 +25,13 @@ export class EmpresaCreateComponent implements OnInit {
   }
 
 
+
   constructor(private empresaService: EmpresaService, private router: Router) { }
 
   ngOnInit(): void {
+    this.empresaService.read().subscribe(empresas=> {
+      this.empresas = empresas;
+    })
   }
 
   togglePassword() {
@@ -50,17 +56,37 @@ export class EmpresaCreateComponent implements OnInit {
 
   createEmpresa(): void {
     let senha = document.getElementById('senha2') as HTMLInputElement;
-    if (!this.validatePassword(this.empresa.senha, senha.value.trim())) {
-      this.empresaService.showMessage("Senhas não conferem");
-      throw new Error('Senhas não conferem');
+    let email = document.getElementById('email') as HTMLInputElement;
+    let cnpj = document.getElementById('cnpj') as HTMLInputElement;
+
+    if (!this.inputValidator()) {
+      this.empresaService.showMessage("Preencha todos os campos!", true);
+      throw new Error('Preencha todos os campos!');
     }
+
     if (this.inputValidator()) {
+      if (!this.validateCnpj(cnpj.value.trim())) {
+        this.empresaService.showMessage("Formato de CNPJ inválido", true);
+        throw new Error('Formato de CNPJ inválido');
+      }
+
+      if (!this.validateEmail(email.value.trim())) {
+        this.empresaService.showMessage("Formato de e-mail inválido.", true);
+        throw new Error('Formato de e-mail inválido.');
+      }
+      if (!this.validatePassword(this.empresa.senha, senha.value.trim())) {
+        this.empresaService.showMessage("Senhas não conferem", true);
+        throw new Error('Senhas não conferem');
+      }
+    }
+
+    if (this.empresas.find(empresa => empresa.email == email.value.trim()) != undefined) {
+      this.empresaService.showMessage('Email cadastrado ou inválido!', true);
+    } else {
       this.empresaService.create(this.empresa).subscribe(() => {
         this.empresaService.showMessage("Empresa cadastrada com sucesso!");
         this.router.navigate(['/']);
       })
-    } else {
-      this.empresaService.showMessage("Preencha todos os campos!");
     }
   }
 
@@ -70,48 +96,46 @@ export class EmpresaCreateComponent implements OnInit {
 
   private inputValidator(): boolean {
     let senha2 = document.getElementById('senha2') as HTMLInputElement;
-    console.log(senha2.value.trim());
-
     if (this.empresa.nome.trim() === '' || this.empresa.email.trim() === '' || this.empresa.cnpj.trim() === '' || this.empresa.endereco.trim() === '' || this.empresa.senha.trim() === '' || senha2.value.trim() === '') {
       if (this.empresa.nome.trim() === '') {
-        document.getElementById('nome').classList.add('required');
+        document.getElementById('nome').classList.add('obrigatory');
       }
       else {
-        document.getElementById('nome').classList.remove('required');
+        document.getElementById('nome').classList.remove('obrigatory');
       }
 
       if (this.empresa.email.trim() === '') {
-        document.getElementById('email').classList.add('required');
+        document.getElementById('email').classList.add('obrigatory');
       }
       else {
-        document.getElementById('email').classList.remove('required');
+        document.getElementById('email').classList.remove('obrigatory');
       }
 
       if (this.empresa.cnpj.trim() === '') {
-        document.getElementById('cnpj').classList.add('required');
+        document.getElementById('cnpj').classList.add('obrigatory');
       }
       else {
-        document.getElementById('cnpj').classList.remove('required');
+        document.getElementById('cnpj').classList.remove('obrigatory');
       }
 
       if (this.empresa.endereco.trim() === '') {
-        document.getElementById('endereco').classList.add('required');
+        document.getElementById('endereco').classList.add('obrigatory');
       }
       else {
-        document.getElementById('endereco').classList.remove('required');
+        document.getElementById('endereco').classList.remove('obrigatory');
       }
 
       if (this.empresa.senha.trim() === '') {
-        document.getElementById('senha').classList.add('required');
+        document.getElementById('senha').classList.add('obrigatory');
       }
       else {
-        document.getElementById('senha').classList.remove('required');
+        document.getElementById('senha').classList.remove('obrigatory');
       }
       if (senha2.value.trim() === '') {
-        document.getElementById('senha2').classList.add('required');
+        document.getElementById('senha2').classList.add('obrigatory');
       }
       else {
-        document.getElementById('senha2').classList.remove('required');
+        document.getElementById('senha2').classList.remove('obrigatory');
       }
       return false;
     }
@@ -123,14 +147,41 @@ export class EmpresaCreateComponent implements OnInit {
 
   private validatePassword(senha: string, senha2: string) {
     if (senha !== senha2) {
-      document.getElementById('senha2').classList.add('required');
+      document.getElementById('senha2').classList.add('obrigatory');
       return false;
     }
     else {
-      document.getElementById('senha2').classList.remove('required');
+      document.getElementById('senha2').classList.remove('obrigatory');
       return true;
     }
   }
 
-}
+  private validateCnpj(cnpj: string): boolean {
+    var cnpjRegex = /^\d{15}$/;
+    if (!cnpjRegex.test(cnpj)) {
+      return false;
+    }
+    return true;
+  }
 
+  private validateEmail(email: string): boolean {
+    if (!this.validateEmailFormat(email)) {
+      document.getElementById('email').classList.add('obrigatory');
+      return false;
+    }
+
+    document.getElementById('email').classList.remove('obrigatory');
+    return true;
+  }
+
+
+  private validateEmailFormat(email: string): boolean {
+    var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    if (!emailRegex.test(email)) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+}
--- frontend/src/app/components/empresa/empresa.service.ts
+++ frontend/src/app/components/empresa/empresa.service.ts
@@ -1,5 +1,5 @@
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
+import { catchError, EMPTY, map, Observable } from 'rxjs';
 import { Empresa } from './empresa.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -9,21 +9,39 @@ import { Injectable } from '@angular/core';
 })
 export class EmpresaService {
 
+
   baseUrl = "http://localhost:3001/empresas/"
 
   constructor(private http: HttpClient, private snackBar: MatSnackBar) { }
 
 
-  create(empresa: Empresa): Observable<Empresa> {
-    return this.http.post<Empresa>(this.baseUrl, empresa);
-  }
-
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void{
     this.snackBar.open(msg, 'X', {
-      duration: 3000,
+      duration: 2000,
       horizontalPosition: "right",
-      verticalPosition: "top"
+      verticalPosition: "top",
+      panelClass: isError ? ['msg-error'] : ['msg-success']
     })
   }
 
-}
+  create(empresa: Empresa): Observable<Empresa> {
+    return this.http.post<Empresa>(this.baseUrl, empresa).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e)));
+  }
+
+  read(): Observable<Empresa[]> {
+    return this.http.get<Empresa[]>(this.baseUrl).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
+  }
+  
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Ocorreu um erro!', true);
+    return EMPTY;
+  }
+ }
+  
+
+
